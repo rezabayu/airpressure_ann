8fc639b4b521c943c2d5c0492038774118d16bfc
@rezabayu <rezabayuperdana@gmail.com>
2025-06-03 11:08:51 +0700

    Bootstrap project structure

---
+++ ann_pressure_prediction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repo for QFE-QFF data science project.',
+    author='rezabayu',
+    license='',
+)
